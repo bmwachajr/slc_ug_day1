9118802259c26883891dcae4b5e5fb8a7f67929a
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-17 17:02:39 +0300

    First Blueprint of function

---
+++ solution.py
@@ -0,0 +1,20 @@
+"""
+At least 5 test cases defined in your public Github repo for the function you are to code
+
+A working function to generate prime numbers from 0 to n with asymptotic analysis in your public Github repo
+
+At least 5 commits on your public Github repo
+"""
+n = 0
+def function(n):
+  a = 1
+  prime_list = []
+  
+  while i <= n:
+    if (a%2 != 0) or(a%3 !=0) or(a%5 !=0) or(a%7 !=0):
+      prime_number.append(a)
+    
+    a += 1
+    i += 1
+    
+  print (prime_list)
