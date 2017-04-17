6cb93e886990bb1ec8dac46b2d3988e0083f60bd
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-17 18:06:55 +0300

    finalized function to print prime numbers

--- solution.py
+++ solution.py
@@ -4,17 +4,44 @@ At least 5 test cases defined in your public Github repo for the function you ar
 A working function to generate prime numbers from 0 to n with asymptotic analysis in your public Github repo
 
 At least 5 commits on your public Github repo
+
 """
-n = 0
+
+"""
+Input: an integer n > 1.
+ 
+ Let A be an array of Boolean values, indexed by integers 2 to n,
+ initially all set to true.
+ 
+ for i = 2, 3, 4, ..., not exceeding vn:
+   if A[i] is true:
+     for j = i2, i2+i, i2+2i, i2+3i, ..., not exceeding n:
+       A[j] := false.
+ 
+ Output: all i such that A[i] is true.
+ 
+"""
 def function(n):
-  a = 1
-  prime_list = []
+  #create empty list
+  prime_list = [2]
+  if n > 1:
+    i = 2
+    while i < n:
+      j = 2
+      while j < i:
+        if (i % j == 0): 
+          break
+        if(i == j+1):
+          prime_list.append(i)
+        j += 1
+      i += 1
+    print (prime_list)
   
-  while i <= n:
-    if (a%2 != 0) or(a%3 !=0) or(a%5 !=0) or(a%7 !=0):
-      prime_number.append(a)
-    
-    a += 1
-    i += 1
-    
-  print (prime_list)
+n = 100
+"""
+tests
+n must be int
+n mustnt be empty
+n must be > 0
+"""
+function(n)
