ab669608561f76bd4f56a31323fabde820a31d28
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-18 08:51:12 +0300

    defined function testcases

---
+++ TestCases.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest import TestCase
+
+class functionTestCases(TestCase):
+  def test_returns_correct_list(self):
+    arg = 10
+    result = function(arg)
+    self.assertEqual(result, [2,3,5,7], msg='Expected {}, got {}'.format([2,3,5,7], result))
+    
+  def test_non_integer_argument(self):
+    with self.assertRaises(ValueError) as context:
+      arg = -10
+      result = function(arg)
+      self.assertEqual(
+        'Argument must be positive interger',
+        context.exception.message,
+        'Only positive integers are allowed as input'
+      )
+  
+  def test_non_integer_argument(self):
+    with self.assertRaises(TypeError) as context:
+      arg = 'arg'
+      result = function(arg)
+      self.assertEqual(
+        'Argument must be interger',
+        context.exception.message,
+        'Only integers are allowed as input'
+      )
