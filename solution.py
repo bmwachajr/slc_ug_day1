5b31600bbd2de27d76330518bb2ac657eefa0be1
Benjamin Lucas Wacha <design@boroborostudio.com>
2017-04-18 08:52:01 +0300

    cleanup function

--- solution.py
+++ solution.py
@@ -21,27 +21,56 @@ Input: an integer n > 1.
  Output: all i such that A[i] is true.
  
 """
-def function(n):
-  #create empty list
-  prime_list = [2]
-  if n > 1:
-    i = 2
-    while i < n:
-      j = 2
-      while j < i:
-        if (i % j == 0): 
-          break
-        if(i == j+1):
-          prime_list.append(i)
-        j += 1
-      i += 1
-    print (prime_list)
+import unittest
+from unittest import TestCase
+
+class functionTestCases(TestCase):
+  def test_returns_correct_list(self):
+    arg = 10
+    result = function(arg)
+    self.assertEqual(result, [2,3,5,7], msg='Expected {}, got {}'.format([2,3,5,7], result))
+    
+  def test_non_integer_argument(self):
+    with self.assertRaises(ValueError) as context:
+      arg = -10
+      result = function(arg)
+      self.assertEqual(
+        'Argument must be positive interger',
+        context.exception.message,
+        'Only positive integers are allowed as input'
+      )
   
-n = 100
-"""
-tests
-n must be int
-n mustnt be empty
-n must be > 0
-"""
-function(n)
+  def test_non_integer_argument(self):
+    with self.assertRaises(TypeError) as context:
+      arg = 'arg'
+      result = function(arg)
+      self.assertEqual(
+        'Argument must be interger',
+        context.exception.message,
+        'Only integers are allowed as input'
+      )
+
+def function(arg):
+  if arg < 0:
+    raise ValueError('Argument must be positive interger')
+  elif isinstance(arg, int):
+    prime_list = [2]
+    if arg > 1:
+      i = 2
+      while i < arg:
+        j = 2
+        while j < i:
+          if (i % j == 0): 
+            break
+          if(i == j+1):
+            prime_list.append(i)
+          j += 1
+        i += 1
+      print (prime_list)
+  else:
+    raise TypeError('Argument must be interger')
+  
+  return prime_list
+  
+if __name__ == '__main__':
+  unittest.main()
